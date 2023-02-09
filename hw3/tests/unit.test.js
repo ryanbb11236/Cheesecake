fab734debf1a610c0d525057414ce3a22fe2df4e
Blaise Bromley <ryanbperiod@gmail.com>
2023-02-08 21:46:08 -0800

    Moved stuff back into the ugly hw3 directory to avoid grader confusion :(

---
+++ hw3/tests/unit.test.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+test('test selectEvent', () => {
+
+    //Read teh index.html file into a string
+    var html = fs.readFileSync('public/index.html', 'utf8');
+    expect(html).toEqual(expect.anything()); //any non-null value is okay
+
+    //put the html into a testing DOM and do a sanity check
+    document.body.innerHTML = html;
+    const $ = require('jquery');
+    expect($('h1').html()).toBe("Cheescake Order Form");
+})
